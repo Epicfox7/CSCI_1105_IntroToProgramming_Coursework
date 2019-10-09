66ce3c6dba2e85eb372ff3f0805b71eadcca718b
John Lopez <56083360+Epicfox7@users.noreply.github.com>
2019-10-09 09:12:28 -0600

    Delete Exercise 1-5.java

--- Exercise 1-5.java
+++
@@ -1,9 +0,0 @@
-class Title Exercise 1.5 {
-	public static void main(String[] args) {
-		/*
-		Author: John Lopez
-		Date: 10/8/2019
-		*/
-		System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
-	}
-}
