ad84acf7cb458aef47eb08a10c299a80358c2d78
John Lopez <56083360+Epicfox7@users.noreply.github.com>
2019-10-09 09:12:11 -0600

    Add files via upload

---
+++ Chapter 1/Exercise 1-9-1.java
@@ -0,0 +1,10 @@
+class Title Exercise 1-9 {
+	public static void main(String[] args) {
+		/*
+		Author: John Lopez
+		Date: 10/08/2019
+		*/
+		System.out.println ("Area = ");
+		System.out.println (4.5 * 7.9);
+	}
+}
