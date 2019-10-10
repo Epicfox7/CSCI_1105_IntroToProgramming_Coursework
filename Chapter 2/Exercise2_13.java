4e8c24f79eac205665013efd853315a1feed8314
John Lopez <56083360+Epicfox7@users.noreply.github.com>
2019-10-10 11:46:01 -0600

    Add files via upload

---
+++ Chapter 2/Exercise2_13.java
@@ -0,0 +1,32 @@
+/*
+Author: John Lopez.
+Date: 10/09/2019.
+*/
+
+import java.util.Scanner;
+
+class Exercise2_13 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		final double MOUNTHLY_INTEREST_RATE = 0.00417;
+		
+		System.out.print("Enter the monthly saving amount:$");
+		double savingAmount = input.nextDouble();
+				
+		// First month
+		double total = savingAmount * (1 + MOUNTHLY_INTEREST_RATE);
+		// Second month
+		total = (savingAmount + total) * (1 + MOUNTHLY_INTEREST_RATE);
+		// Third month 
+		total = (savingAmount + total) * (1 + MOUNTHLY_INTEREST_RATE);
+		// Fourth month
+		total = (savingAmount + total) * (1 + MOUNTHLY_INTEREST_RATE);
+		// Fifth month 
+		total = (savingAmount + total) * (1 + MOUNTHLY_INTEREST_RATE);
+		// Sixth month 
+		total = (savingAmount + total) * (1 + MOUNTHLY_INTEREST_RATE);
+		
+		System.out.println("In the Sixth month the value is $" + total);
+	}
+}
---
+++ Chapter 2/Exercise2_23.java
@@ -0,0 +1,24 @@
+/*
+Author: John Lopez.
+Date:10/10/2019
+*/
+
+import java.util.Scanner;
+
+class Exercise2_23 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the distance to drive: ");
+		double distance = input.nextDouble();
+		System.out.print("Enter the miles per gallon: ");
+		double milesPerGallon = input.nextDouble();
+		System.out.print("Enter the price per gallon: $");
+		double pricePerGallon = input.nextDouble();
+		
+		double costOfDriving = ((distance / milesPerGallon) * pricePerGallon);
+		
+		System.out.println(("The cost of drivig is: $") + costOfDriving);
+	}
+}
