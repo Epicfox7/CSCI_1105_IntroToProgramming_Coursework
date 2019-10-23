5b2571c99a09c085cbf02cd496d57d7d934ad3b8
John Lopez <56083360+Epicfox7@users.noreply.github.com>
2019-10-23 11:56:01 -0600

    Add files via upload

---
+++ Chapter 4/Exercise4_23.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Untitled {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter employee's name: ");
+		String name = input.nextLine();
+		System.out.print("Enter the number of hours worked: ");
+		String hours = input.nextLine();
+		System.out.print("Enter the hourly pay rate: ");
+		String hourlypay = input.nextLine();
+		System.out.print("Enter the federal tax withholding rate: ");
+		String ftaxrate = input.nextInt();
+		System.out.print("Enter the state tax withholding rate: ");
+		String staxrate = input.nextInt();
+		
+		double tdeduction = ftaxrate + staxrate;
+		double grosspay = hours * hourlypay;
+		double netpay = (((hours * hourlypay) - 20.00%) - 9.0%);
+		
+		System.out.print("Employee Name: "+ name);
+		System.out.print("Hours Worked: "+ hours);
+		System.out.print("Pay Rate:$ "+ hourlypay);
+		System.out.print("Gross Pay:$ "+ grosspay);
+		System.out.print("Net Pay:$ "+ netpay);
+		
+	}
+}
