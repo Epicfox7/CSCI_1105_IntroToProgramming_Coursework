a80ddec2d8fb4e784776e60fd76a89add88826e6
John Lopez <56083360+Epicfox7@users.noreply.github.com>
2019-10-23 11:13:56 -0600

    Add files via upload

---
+++ Chapter 4/Exercise4_1.java
@@ -0,0 +1,20 @@
+/*
+Author: John Lopez.
+Date: 10/23/2019.
+*/
+
+import java.util.Scanner;
+
+class Exercise4_1 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the length from the center to the vertex: ");
+		double r = input.nextDouble();
+		double s = (2 * r) * Math.sin(Math.PI / 5);
+		double area = (5 * Math.pow(s , 2)) / (4 * Math.tan(Math.PI / 5));
+		
+		System.out.println(" ");
+		System.out.printf("The area of the pentagon is %.2f\n ", area);
+	}
+}
