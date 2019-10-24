1f642ed44625d313f2b853d42ecf267e83fb1aa0
John Lopez <56083360+Epicfox7@users.noreply.github.com>
2019-10-24 11:17:57 -0600

    Update Exercise4_23.java

--- Chapter 4/Exercise4_23.java
+++ Chapter 4/Exercise4_23.java
@@ -1,31 +1,35 @@
 import java.util.Scanner;
 
-class Untitled {
+class Exercise4_23 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		
 		
 		System.out.print("Enter employee's name: ");
 		String name = input.nextLine();
 		System.out.print("Enter the number of hours worked: ");
-		String hours = input.nextLine();
-		System.out.print("Enter the hourly pay rate: ");
-		String hourlypay = input.nextLine();
+		double hours = input.nextDouble();
+		System.out.print("Enter the hourly pay rate: $");
+		double hourlypay = input.nextDouble();
 		System.out.print("Enter the federal tax withholding rate: ");
-		String ftaxrate = input.nextInt();
+		double ftaxrate = input.nextDouble();
 		System.out.print("Enter the state tax withholding rate: ");
-		String staxrate = input.nextInt();
+		double staxrate = input.nextDouble();
 		
-		//missing the federal and state tax rates %
-		
-		double tdeduction = ftaxrate + staxrate;
 		double grosspay = hours * hourlypay;
-		double netpay = (((hours * hourlypay) - 20.00%) - 9.0%);
+		double netpay = grosspay;
+		double ftaxrate2 = grosspay * ftaxrate;
+		double staxrate2 = grosspay * staxrate;		
 		
-		System.out.print("Employee Name: "+ name);
-		System.out.print("Hours Worked: "+ hours);
-		System.out.print("Pay Rate:$ "+ hourlypay);
-		System.out.print("Gross Pay:$ "+ grosspay);
-		System.out.print("Net Pay:$ "+ netpay);
-		
+		System.out.println(" ");
+		System.out.println("Employee Name: "+ name);
+		System.out.println("Hours Worked: "+ hours);
+		System.out.printf("Pay Rate: $%.2f\n ", hourlypay);
+		System.out.printf("Gross Pay: $%.2f\n ", grosspay);
+		System.out.println("Deductions:");
+		System.out.printf("		Federal Withholding (20.0%): $.2\n ", (int)(ftaxrate2 * 100 / 100.00));
+		System.out.printf("        State Withholding (9.0%): $.2f\n ", (int)(staxrate2 * 100 / 100.00));
+		System.out.println("        Total Deduction:");
+		System.out.printf("Net Pay: $%.2f\n ", netpay);							
 	}
 }
