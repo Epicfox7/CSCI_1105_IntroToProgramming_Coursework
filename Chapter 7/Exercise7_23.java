9c5aab65844bdf5b529f8c8058b893cb3c1c0074
John Lopez <56083360+Epicfox7@users.noreply.github.com>
2020-01-06 10:30:04 -0700

    Add files via upload

---
+++ Chapter 7/Exercise7_23.java
@@ -0,0 +1,53 @@
+/*
+Author: John Lopez.
+Date: 01/06/2020.
+*/
+
+import java.util.Scanner;
+
+class Exercise7_23 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String[] lockers = new String[100];
+		
+		lockerClosed(lockers);
+		lockersKey(lockers);
+		lockersPrint(lockers);
+		
+	}
+	
+	public static boolean lockerOpened(String lockerOpnd){
+		return lockerOpnd == "Opened";
+	}
+	
+	public static void lockerClosed(String[] lockers){
+		
+		for (int i = 0;i < lockers.length; i++){
+			lockers[i] = "Closed";
+		}
+	}
+	
+	public static void lockersKey(String[] lockers){
+		
+		int list = 0;
+		
+		for(int l = 1; l <= lockers.length; l++){
+			for(int k = 0; k < lockers.length; k += l){
+				if(lockerOpened(lockers[k]))
+					lockers[k] = "Closed";
+				else
+					lockers[k] = "Opened";
+				}
+			list ++;
+		}
+	}
+	
+	public static void lockersPrint(String[] lockers){
+		
+		for(int i = 0;i < lockers.length; i++){
+			if(lockerOpened(lockers[i]))
+				System.out.print("Locker " + (i + 1) + ", ");
+			}
+		System.out.println();
+	}
+}
