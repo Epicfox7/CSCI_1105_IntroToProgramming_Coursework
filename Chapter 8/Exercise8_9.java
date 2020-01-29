b7a2ab72d955ce361de4016887302b33b8d8b75f
John Lopez <56083360+Epicfox7@users.noreply.github.com>
2020-01-29 09:18:43 -0700

    Update Exercise8_9.java

--- Chapter 8/Exercise8_9.java
+++ Chapter 8/Exercise8_9.java
@@ -32,7 +32,7 @@ class Exercise8_9 {
 				System.out.print("Enter a column (0, 1, or 2) for player X: ");
 				int columnX = input.nextInt();
 				if ("O".equals(tictactoe[rowX][columnX])){
-					System.out.print("Error");
+					System.out.print("\nError!\n");
 					continue;
 				}
 				tictactoe[rowX][columnX] = "X";
@@ -44,7 +44,7 @@ class Exercise8_9 {
 				System.out.print("Enter a column (0, 1, or 2) for player O: ");
 				int columnO = input.nextInt();
 				if ("X".equals(tictactoe[rowO][columnO])){
-					System.out.print("Error");
+					System.out.print("\nError!\n");
 					continue;
 				}
 				tictactoe[rowO][columnO] = "O";
